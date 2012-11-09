5d1441d71c01cbbbc3c69f6c616875fb2d1910f4
Anis Moubarik <moubarik@cs.helsinki.fi>
2012-11-09 21:04:32 +0200

    prime

--- cpp2week/week2.cpp
+++ cpp2week/week2.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+bool isPrime(int checkthis){
+
+	for(int i = 2; i < checkthis; i++){
+		if(checkthis % i== 0){
+			return false;
+		}
+	}
+
+	return true;
+
+}
+
 int main(){
 	cout << "Hello world\n";
 
@@ -23,7 +35,20 @@ int main(){
 		cout << *it << "\n";
 	}
 
-	cout << "Press any key to exit\n";
+	int isThisAPrime = 0;
+
+	cout << "Give me an number, and I'll check if it is an prime number\n";
+	cin >> isThisAPrime;
+
+	bool isAPrime = isPrime(isThisAPrime);
+
+	if(isAPrime == true){
+		cout << "The number is a prime\n";
+	}else{
+		cout << "The nume is not a prime\n";
+	}
+
+	cout<<"\n";
 	cin.get();
 	return 0;
 }
