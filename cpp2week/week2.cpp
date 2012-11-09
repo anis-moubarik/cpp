10797d8ce0cbd8b975525a2d3b42395af37bc3c5
Anis Moubarik <moubarik@cs.helsinki.fi>
2012-11-09 22:52:12 +0200

    calculator

--- cpp2week/week2.cpp
+++ cpp2week/week2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "Calc.h"
 
 using namespace std;
 
@@ -17,38 +18,53 @@ bool isPrime(int checkthis){
 }
 
 int main(){
-	cout << "Hello world\n";
+	//cout << "Hello world\n";
 
-	string s;
-	cout << "Give me your name\n";
-	getline(cin, s);
-	cout <<"Hello " << s << "\n";
+	//string s;
+	//cout << "Give me your name\n";
+	//getline(cin, s);
+	//cout <<"Hello " << s << "\n";
 
-	vector<int> numbers;
-	for(int i = 0; i < 3; i++){
-		numbers.push_back(i);
+	//vector<int> numbers;
+	//for(int i = 0; i < 3; i++){
+	//	numbers.push_back(i);
+	//}
+
+	//vector<int>::iterator it;
+
+	//for(it=numbers.begin(); it < numbers.end(); it++){
+	//	cout << *it << "\n";
+	//}
+
+	//int isThisAPrime = 0;
+
+	//cout << "Give me an number, and I'll check if it is an prime number\n";
+	//cin >> isThisAPrime;
+
+	//bool isAPrime = isPrime(isThisAPrime);
+
+	//if(isAPrime == true){
+	//	cout << "The number is a prime\n";
+	//}else{
+	//	cout << "The nume is not a prime\n";
+	//}
+
+	//cout<<"\n";
+	//cin.get();
+
+	Calc calc;
+
+	cout << calc.calculate("+ 100 3.13 4") << "\n";
+
+	string calculations;
+	while(true){
+		cout << "Give us something to calculate. (type quit to quit)\n";
+		getline(cin, calculations);
+		if(calculations == "quit"){
+			break;
+		}
+		cout << "The result is " << calc.calculate(calculations) << "\n";
 	}
 
-	vector<int>::iterator it;
-
-	for(it=numbers.begin(); it < numbers.end(); it++){
-		cout << *it << "\n";
-	}
-
-	int isThisAPrime = 0;
-
-	cout << "Give me an number, and I'll check if it is an prime number\n";
-	cin >> isThisAPrime;
-
-	bool isAPrime = isPrime(isThisAPrime);
-
-	if(isAPrime == true){
-		cout << "The number is a prime\n";
-	}else{
-		cout << "The nume is not a prime\n";
-	}
-
-	cout<<"\n";
-	cin.get();
 	return 0;
 }
